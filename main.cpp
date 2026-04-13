2f538ea0534948508f7d69ed19891bd3664ba240
Loma <lomakim@gmail.com>
2026-04-12 19:59:44 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -23,11 +23,13 @@ int main() {
     auto start = high_resolution_clock::now();
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<nanoseconds>(end - start);
+    auto it = list.begin();
+    auto it2 = set.begin();
 
 
     for (int i = 0; i < 15; i++) {
         //READING FILE
-        start = high_resolution_clock::now();                   //vector
+        start = high_resolution_clock::now();                   //vector read
         while (fin >> temp) {
             vec.push_back(temp);
         }
@@ -35,7 +37,7 @@ int main() {
         duration = duration_cast<nanoseconds>(end - start);
         times[0][0][i] = duration.count();
 
-        start = high_resolution_clock::now();                   // list
+        start = high_resolution_clock::now();                   // list read
         while (fin >> temp) {
             list.push_back(temp);
         }
@@ -43,7 +45,7 @@ int main() {
         duration = duration_cast<nanoseconds>(end - start);
         times[1][0][i] = duration.count();
 
-        start = high_resolution_clock::now();                   // set
+        start = high_resolution_clock::now();                   // set read
         while (fin >> temp) {
             set.insert(temp);
         }
@@ -52,70 +54,68 @@ int main() {
         times[2][0][i] = duration.count();
 
         //SORTING FILE
-        start = high_resolution_clock::now();
+        start = high_resolution_clock::now();                   // vector sort
         sort(vec.begin(), vec.end());
         end = high_resolution_clock::now();
         duration = duration_cast<nanoseconds>(end - start);
         times[0][1][i] = duration.count();
 
-        start = high_resolution_clock::now();
+        start = high_resolution_clock::now();                   // list sort
         list.sort();
         end = high_resolution_clock::now();
         duration = duration_cast<nanoseconds>(end - start);
         times[1][1][i] = duration.count();
 
-        times[2][1][i] = -1;
+        times[2][1][i] = -1;                                    // set "sort"
+
+        //INSERTING
+        string t = "TESTCODE";
+
+        start = high_resolution_clock::now();                   // vector insert
+        vec.insert(vec.begin() + (vec.size() / 2), t);
+        end = high_resolution_clock::now();
+        duration = duration_cast<nanoseconds>(end - start);
+        times[0][2][i] = duration.count();;
+        
+        start = high_resolution_clock::now();                   // list insert
+        it = list.begin();
+        advance(it, (list.size() / 2));
+        list.insert(it, t);
+        end = high_resolution_clock::now();
+        duration = duration_cast<nanoseconds>(end - start);
+        times[1][2][i] = duration.count();
+
+        start = high_resolution_clock::now();                   // set insert
+        set.insert(t);
+        end = high_resolution_clock::now();
+        duration = duration_cast<nanoseconds>(end - start);
+        times[2][2][i] = duration.count();
+
+        //DELETING
+        start = high_resolution_clock::now();                   // vector delete
+        vec.erase(vec.begin() + (vec.size() / 2));
+        end = high_resolution_clock::now();
+        duration = duration_cast<nanoseconds>(end - start);
+        times[0][3][i] = duration.count();
+
+
+        start = high_resolution_clock::now();                   // list delete
+        it = list.begin();
+        advance(it, (list.size() / 2));
+        list.erase(it);
+        end = high_resolution_clock::now();
+        duration = duration_cast<nanoseconds>(end - start);
+        times[1][3][i] = duration.count();
+
+        start = high_resolution_clock::now();                   // set delete
+        it2 = set.begin();
+        advance(it2, (set.size() / 2));
+        set.erase(it2);
+        end = high_resolution_clock::now();
+        duration = duration_cast<nanoseconds>(end - start);
+        times[2][3][i] = duration.count();
     }
-
-
-    //INSERTING
-    string t = "TESTCODE";
-
-    start = high_resolution_clock::now();
-    vec.insert(vec.begin() + (vec.size() / 2), t);
-    end = high_resolution_clock::now();
-    duration = duration_cast<nanoseconds>(end - start);
-    ins.vect = duration.count();;
-    
-    start = high_resolution_clock::now();
-    auto it = list.begin();
-    advance(it, (list.size() / 2));
-    list.insert(it, t);
-    end = high_resolution_clock::now();
-    duration = duration_cast<nanoseconds>(end - start);
-    ins.list= duration.count();
-
-    start = high_resolution_clock::now();
-    set.insert(t);
-    end = high_resolution_clock::now();
-    duration = duration_cast<nanoseconds>(end - start);
-    ins.set = duration.count();
-
-    //DELETING
-    start = high_resolution_clock::now();
-    vec.erase(vec.begin() + (vec.size() / 2));
-    end = high_resolution_clock::now();
-    duration = duration_cast<nanoseconds>(end - start);
-    del.vect = duration.count();
-
-
-    start = high_resolution_clock::now();
-    it = list.begin();
-    advance(it, (list.size() / 2));
-    list.erase(it);
-    end = high_resolution_clock::now();
-    duration = duration_cast<nanoseconds>(end - start);
-    del.list = duration.count();
-
-    start = high_resolution_clock::now();
-    auto it2 = set.begin();
-    advance(it2, (set.size() / 2));
-    set.erase(it2);
-    end = high_resolution_clock::now();
-    duration = duration_cast<nanoseconds>(end - start);
-    del.set = duration.count();
-
-    fin.close();
+    fin.close();    // close file
 
     cout << "**** DATA STRUCTURE RACE RESULTS ****" << endl;
     cout << setw(WIDTH) << "Operation";
@@ -124,19 +124,19 @@ int main() {
     cout << setw(WIDTH) << "Set" << endl;
 
     cout << setw(WIDTH) << "Read";
-    cout << setw(WIDTH) << read.vect;
-    cout << setw(WIDTH) << read.list;
-    cout << setw(WIDTH) << read.set << endl;
+    cout << setw(WIDTH) << times[0][0][0];
+    cout << setw(WIDTH) << times[0][0][0];
+    cout << setw(WIDTH) << times[0][0][0] << endl;
 
     cout << setw(WIDTH) << "Sort";
-    cout << setw(WIDTH) << sort1.vect;
-    cout << setw(WIDTH) << sort1.list;
-    cout << setw(WIDTH) << sort1.set << endl;
+    cout << setw(WIDTH) << times[0][0][0];
+    cout << setw(WIDTH) << times[0][0][0];
+    cout << setw(WIDTH) << times[0][0][0] << endl;
 
     cout << setw(WIDTH) << "Delete";
-    cout << setw(WIDTH) << del.vect;
-    cout << setw(WIDTH) << del.list;
-    cout << setw(WIDTH) << del.set;
+    cout << setw(WIDTH) << times[0][0][0];
+    cout << setw(WIDTH) << times[0][0][0];
+    cout << setw(WIDTH) << times[0][0][0];
 
     return 0;
 }
